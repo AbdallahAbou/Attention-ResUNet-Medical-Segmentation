a747ecabd6b3adc0c2033ef65b609599a567ffcf
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-09-30 16:23:01 +0200

    Edit dirs

--- setup.sh
+++ setup.sh
@@ -10,11 +10,16 @@
 JOB_DIR=${1:-"./"}
 DOWNLOAD_DIR=${2:-"./"}
 OUTPUT_DIR=${3:-"./output"}
-DATASETS_DIR=${4:-"./data"}
+DATASETS_DIR=${4:-"./dataset"}
 
 mkdir $JOB_DIR $DOWNLOAD_DIR $OUTPUT_DIR $DATASETS_DIR
 
 ###--- YOUR CODE FROM HERE ---###
+
+mkdir -p $DATASETS_DIR/raw
+mkdir -p $DATASETS_DIR/interim
+mkdir -p $DATASETS_DIR/processed
+
 
 echo "Hello World!"
 echo "The job directory is $JOB_DIR"
