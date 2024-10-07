6599363383c376d75d62bcccaf1878c1796beee8
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-10-07 19:23:21 +0200

    Add batch_size parameter

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,7 +5,7 @@ from src.data.data_loader import MedicalDataset, custom_collate_fn
 from src.models.attention_res_unet import AttentionResUNet
 from src.models.loss import DiceLoss  
 
-def train_model(train_images_dir, train_labels_dir, model_save_path, val_split=0.2, num_epochs=10, learning_rate=1e-4, pretrained_model_path=None):
+def train_model(train_images_dir, train_labels_dir, model_save_path, val_split=0.2, batch_size=1, num_epochs=10, learning_rate=1e-4, pretrained_model_path=None):
     """
     Trains the AttentionResUNet model with the provided datasets.
 
@@ -23,7 +23,7 @@ def train_model(train_images_dir, train_labels_dir, model_save_path, val_split=0
 
     # Set device (GPU/CPU)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
+    print(f"Using device: {torch.cuda.get_device_name(0)}")
     # Create dataset
     full_dataset = MedicalDataset(image_dir=train_images_dir, label_dir=train_labels_dir)
 
@@ -33,8 +33,8 @@ def train_model(train_images_dir, train_labels_dir, model_save_path, val_split=0
     train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
     # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=custom_collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=custom_collate_fn)
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, collate_fn=custom_collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size, shuffle=False, collate_fn=custom_collate_fn)
 
     # Initialize model, optimizer, and loss function
     model = AttentionResUNet(in_channels=1, out_channels=2).to(device)
