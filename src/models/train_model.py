1b4e4ab61f353a68ee23567835bffe84c3cbf974
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-10-28 13:36:01 +0100

    Fit data loader ids

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,86 +1,136 @@
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader, random_split
-from src.data.data_loader import MedicalDataset, custom_collate_fn
+from torch.utils.data import DataLoader
+from src.data.data_loader import MedicalSliceDataset
+from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
+from torch.amp import autocast, GradScaler
 from src.models.attention_res_unet import AttentionResUNet
-from src.models.loss import DiceLoss  
+from torch.utils.data import random_split
+import time
+import random
+import os
 
-def train_model(train_images_dir, train_labels_dir, model_save_path, val_split=0.2, batch_size=1, num_epochs=10, learning_rate=1e-4, pretrained_model_path=None):
+
+
+
+
+
+def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epochs=1, learning_rate=1e-4):
     """
     Trains the AttentionResUNet model with the provided datasets.
 
     Parameters:
-    - train_images_dir: str, path to the training images directory.
-    - train_labels_dir: str, path to the training labels directory.
+    - images_dir: str, path to the training images directory.
+    - labels_dir: str, path to the training labels directory.
     - model_save_path: str, path to save the trained model.
-    - val_split: float, the fraction of the dataset to use for validation. Defaults to 0.2 (20%).
     - num_epochs: int, the number of epochs to train for. Defaults to 10.
     - learning_rate: float, the learning rate for the optimizer. Defaults to 1e-4.
-    - pretrained_model_path: str, optional path to a pre-trained model to continue training.   
     Returns:
     - None. Prints training progress and saves the model.
     """
-
-    # Set device (GPU/CPU)
+        
+    # Device setup
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {torch.cuda.get_device_name(0)}")
-    # Create dataset
-    full_dataset = MedicalDataset(image_dir=train_images_dir, label_dir=train_labels_dir)
 
-    # Split dataset into training and validation sets
-    train_size = int((1 - val_split) * len(full_dataset))
-    val_size = len(full_dataset) - train_size
-    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    # Dataset and DataLoader
+    ids_list_ = []
+    for file in sorted(os.listdir(images_dir)):
+        if not file.startswith('._') and file.endswith(".nii.gz"): 
+            ids_list_.append(file)
 
-    # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True, collate_fn=custom_collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size, shuffle=False, collate_fn=custom_collate_fn)
+    random.seed(42) 
+    random.shuffle(ids_list_)
 
-    # Initialize model, optimizer, and loss function
-    model = AttentionResUNet(in_channels=1, out_channels=2).to(device)
+    train_size = int(0.8 * len(ids_list_))
+    train_ids = ids_list_[:train_size]
+    val_ids = ids_list_[train_size:]
+    print(f"Training Set length: {len(train_ids)}/{len(ids_list_)}" )
+    print(f"Validation Set length: {len(val_ids)}/{len(ids_list_)}" )
 
-    if pretrained_model_path:
-        model.load_state_dict(torch.load(pretrained_model_path))
-        print(f"Loaded pre-trained model from {pretrained_model_path}")
-    
+    train_dataset = MedicalSliceDataset(image_dir=images_dir, label_dir=labels_dir, ids_list=train_ids)
+    val_dataset = MedicalSliceDataset(image_dir=images_dir, label_dir=labels_dir, ids_list=val_ids)
+
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=8, drop_last=True)
+
+    # Model, optimizer, and loss function
+    model = AttentionResUNet(in_channels=1, out_channels=3, init_features=16).to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    dice_loss = DiceLoss()
+    criterion = DiceLoss(to_onehot_y=True, softmax=True)
+    dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=True, num_classes=3)
+    scaler = GradScaler('cuda')
 
-    # Training loop
+    best_val_loss = float('inf')
+    patience = 7  # Number of epochs to wait before stopping
+    trigger_times = 0
+
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0
-        for images, labels in train_loader:
+        start_epoch = time.time()
+        for i, (images, labels) in enumerate(train_loader):
+            start_iter = time.time()
+            
             images = images.to(device)
             labels = labels.to(device)
 
             optimizer.zero_grad()
-            outputs = model(images)
 
-            # Calculate loss
-            loss = dice_loss(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            with autocast('cuda'):
+                outputs = model(images)
+                #print(f"Outputs stats - min: {outputs.min()}, max: {outputs.max()}, mean: {outputs.mean()}")
+                loss = criterion(outputs, labels)
+                
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
             train_loss += loss.item()
+            end_iter = time.time()
+            #print(f"Iteration {i+1}/{len(train_loader)}, Elapsed time: {end_iter - start_epoch:.4f} seconds")
 
+        end_epoch = time.time()
         avg_train_loss = train_loss / len(train_loader)
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_train_loss}")
-
-        # Validation
+        # Validation Loop
         model.eval()
+        val_loss = 0
+        dice_metric.reset()
         with torch.no_grad():
-            val_loss = 0
             for images, labels in val_loader:
                 images = images.to(device)
                 labels = labels.to(device)
+
                 outputs = model(images)
-                loss = dice_loss(outputs, labels)
+                loss = criterion(outputs, labels)
                 val_loss += loss.item()
 
-            avg_val_loss = val_loss / len(val_loader)
-            print(f"Epoch [{epoch+1}/{num_epochs}], Validation Loss: {avg_val_loss}")
-    
-    # Save the model
+                outputs_softmax = torch.softmax(outputs, dim=1)
+                #print('Label shape before one hot:', labels.shape)
+                # One-hot encode labels
+                labels_one_hot = torch.nn.functional.one_hot(labels.squeeze(1), num_classes=3)
+                #print('Label shape after one hot:', labels_one_hot.shape)
+                labels_one_hot = labels_one_hot.permute(0, 3, 1, 2).float()
+                #print('Label shape after permute:', labels_one_hot.shape)
+                dice_metric(outputs_softmax, labels_one_hot)
+
+        avg_val_loss = val_loss / len(val_loader)
+        dice_score, not_nans = dice_metric.aggregate()#[0].item()
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Dice score: {dice_score}, Time: {end_epoch - start_epoch:.2f}s")
+        
+        dice_metric.reset()
+
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            trigger_times = 0
+
+        else:
+            trigger_times += 1
+            if trigger_times >= patience:
+                print('Early stopping!')
+                break
     torch.save(model.state_dict(), model_save_path)
 
