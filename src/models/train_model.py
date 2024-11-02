6379e79b3e88bc607608c275ebaec2aab8da9a0f
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-11-02 12:11:51 +0100

    Fix dice metric

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,7 +2,8 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from src.data.data_loader import MedicalSliceDataset
-from monai.losses import DiceLoss
+from src.data.process_data import save_images_under_300_slices
+from monai.losses import DiceLoss, DiceCELoss
 from monai.metrics import DiceMetric
 from monai.networks.utils import one_hot
 from torch.amp import autocast, GradScaler
@@ -12,6 +13,16 @@ import time
 import random
 import os
 import json
+
+def compute_dice(preds, targets, smooth=1e-5):
+    preds = preds > 0.5  
+    preds = preds.float()
+    targets = targets.float()
+    intersection = (preds * targets).sum(dim=(1, 2, 3))  
+    union = preds.sum(dim=(1, 2, 3)) + targets.sum(dim=(1, 2, 3))  
+    dice = (2.0 * intersection + smooth) / (union + smooth)  
+    return dice.mean().item()
+
 
 def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epochs=1, learning_rate=1e-4, preloaded_model_path=None):
     """
@@ -30,10 +41,12 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
     # Device setup
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {torch.cuda.get_device_name(0)}")
-    with open(os.path.join(images_dir, 'images_under_300_slices.json'), 'r') as f:
+    json_300 = os.path.join(images_dir, 'images_under_300_slices.json') 
+    #save_images_under_300_slices(images_dir, json_300)
+    with open(json_300, 'r') as f:
         all_images_under_300 = json.load(f)
-    #ids_list_ = random.sample(all_images_under_300, 30)
     ids_list_ = all_images_under_300
+    ids_list_ = random.sample(all_images_under_300, 15)
     # Dataset and DataLoader
     #ids_list_ = []
     #for file in sorted(os.listdir(images_dir)):
@@ -59,12 +72,13 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=True)
 
     # Model, optimizer, and loss function
-    model = AttentionResUNet(in_channels=1, out_channels=3, init_features=8).to(device)
+    model = AttentionResUNet(in_channels=1, out_channels=1, init_features=8).to(device)
     if preloaded_model_path is not None:
         model.load_state_dict(torch.load(preloaded_model_path))
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = DiceLoss(to_onehot_y=True, softmax=True)
-    dice_metric = DiceMetric(include_background=False, reduction="mean_batch", get_not_nans=False, ignore_empty=False, num_classes=3)
+    Dice_criterion = DiceLoss(to_onehot_y=False, sigmoid=True)
+    criterion = DiceCELoss(sigmoid=True, lambda_dice=0.5, lambda_ce=0.5)
+    dice_metric = DiceMetric(include_background=False, reduction="mean_batch", get_not_nans=False, ignore_empty=False, num_classes=2)
     scaler = GradScaler('cuda')
 
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, verbose=True)
@@ -83,6 +97,8 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
             images = images.to(device)
             labels = labels.to(device)
 
+            labels = (labels != 0).float()
+
             optimizer.zero_grad()
 
             with autocast('cuda'):
@@ -97,32 +113,31 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
             train_loss += loss.item()
             end_iter = time.time()
             print(f"Iteration {i+1}/{len(train_loader)}, Elapsed time: {end_iter - start_epoch:.4f} seconds")
+        #print(f"Training loop: Image shape : {images.shape}, Labels shape : {labels.shape}")
 
         end_epoch = time.time()
         avg_train_loss = train_loss / len(train_loader)
         # Validation Loop
         model.eval()
         val_loss = 0
-        dice_metric.reset()
+        dice_scores_custom = []
         with torch.no_grad():
             for images, labels in val_loader:
                 images = images.to(device)
-                labels = labels.to(device).long()
-
+                labels = labels.to(device)
+                labels = (labels != 0).float()
+                
                 outputs = model(images)
                 loss_ = criterion(outputs, labels)
                 val_loss += loss_.item()
 
-                outputs_softmax = torch.softmax(outputs, dim=1)
-                # One-hot encode labels
-                labels_one_hot = one_hot(labels, num_classes=3)
-                dice_metric(y_pred=outputs_softmax, y=labels_one_hot)
-
+                outputs_sigmoid = torch.sigmoid(outputs)
+                dice_score = compute_dice(outputs_sigmoid, labels)
+                dice_scores_custom.append(dice_score)
         avg_val_loss = val_loss / len(val_loader)
-        dice_scores = dice_metric.aggregate()
-        dice_metric.reset()
-
-        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Dice scores: {dice_scores}, Time: {end_epoch - start_epoch:.2f}s")
+        avg_dice_score = sum(dice_scores_custom) / len(dice_scores_custom)
+        #print(f"Validation loop: Image shape : {images.shape}, Labels shape : {labels.shape}")
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}, Dice score: {avg_dice_score:.4f}, Time: {end_epoch - start_epoch:.2f}s")
 
     
         scheduler.step(avg_val_loss)
