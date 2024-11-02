b18d488583bd6a9a35a24cb29eeafc455a23217d
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-11-02 08:10:01 +0100

    Test

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ liver_train_dir = os.path.join(datasets_dir,"raw/Task03_Liver/imagesTr")
 vessels_labels_dir = os.path.join(datasets_dir,"raw/Task08_HepaticVessel/labelsTr")
 vessels_train_dir = os.path.join(datasets_dir,"processed/Task08_HepaticVessel/imagesTr")
 
-liver_model_save_path = os.path.join(output_dir, "liver_model_real.pth")
+liver_model_save_path = os.path.join(output_dir, "liver_model_real2.pth")
 vessel_model_save_path = os.path.join(output_dir, "vessel_model.pth")
 
 flag_dir = os.path.join(datasets_dir, 'flag.txt')
@@ -174,7 +174,7 @@ flag_dir = os.path.join(datasets_dir, 'flag.txt')
 
 # Call functions to download data and process it
 if check_flag_status(flag_dir) == False:
-    #download_and_prepare_data(download_dir, os.path.join(datasets_dir, 'raw'))
+    download_and_prepare_data(download_dir, os.path.join(datasets_dir, 'raw'))
     #process_all_data(raw_data_dirs, processed_data_dirs)
     set_flag_status(flag_dir)
 else:
@@ -183,5 +183,7 @@ else:
 
 #print_directory_structure(download_dir)
 
-train_model(liver_train_dir, liver_labels_dir, liver_model_save_path, batch_size=128, num_epochs=50, learning_rate=1e-4) 
+pre_model_path = os.path.join(output_dir, "liver_model_real.pth")
+
+train_model(liver_train_dir, liver_labels_dir, liver_model_save_path, batch_size=128, num_epochs=1, learning_rate=1e-4, preloaded_model_path=None) 
 
