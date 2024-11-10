bc178294301d1880675016c9c0f87b1f4b019026
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2024-11-10 21:10:29 +0100

    Reduce model size

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -65,7 +65,7 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {torch.cuda.get_device_name(0)}")
     json_300 = os.path.join(images_dir, 'images_under_300_slices.json') 
-    save_images_under_300_slices(images_dir, json_300, slice_threshold=300)
+    #save_images_under_300_slices(images_dir, json_300, slice_threshold=300)
     with open(json_300, 'r') as f:
         all_images_under_300 = json.load(f)
     ids_list_ = all_images_under_300
@@ -97,7 +97,7 @@ def train_model(images_dir, labels_dir, model_save_path, batch_size=4, num_epoch
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=True)
 
     # Model, optimizer, and loss function
-    model = AttentionResUNet(in_channels=1, out_channels=3, init_features=64).to(device)
+    model = AttentionResUNet(in_channels=1, out_channels=3, init_features=32).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     Dice_criterion = DiceLoss(to_onehot_y=True, softmax=True)
