1fa2bbb9c8ed4e39a8673cab819c822fdcd346af
Abdallah Abouabdallah <abdallah.abouabdallah.bmt@gmail.com>
2025-04-02 10:49:25 +0200

    Add AttentionResUnet Architechture

---
+++ CT_Viewer_Trame/modules/AttentionResUnet.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Residual Block for the segmentation model
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        # Skip connection
+        self.skip_connection = nn.Sequential()
+        if in_channels != out_channels:
+            self.skip_connection = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = self.skip_connection(x)
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += identity
+        return self.relu(out)
+
+# Attention Block for the segmentation model
+class AttentionBlock(nn.Module):
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionBlock, self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(F_int)
+        )
+        
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(F_int)
+        )
+        
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
+
+# U-Net with Attention and Residual Blocks
+class AttentionResUNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=3, init_features=32):
+        super(AttentionResUNet, self).__init__()
+        features = init_features
+
+        # Encoder (with Residual Blocks)
+        self.encoder1 = ResidualBlock(in_channels, features)
+        self.encoder2 = ResidualBlock(features, features * 2)
+        self.encoder3 = ResidualBlock(features * 2, features * 4)
+        self.encoder4 = ResidualBlock(features * 4, features * 8)
+
+        # Bottleneck
+        self.bottleneck = ResidualBlock(features * 8, features * 16)
+
+        # Decoder (with Attention Blocks)
+        self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
+        self.attention4 = AttentionBlock(F_g=features * 8, F_l=features * 8, F_int=features * 4)
+        self.decoder4 = ResidualBlock(features * 16, features * 8)
+
+        self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size=2, stride=2)
+        self.attention3 = AttentionBlock(F_g=features * 4, F_l=features * 4, F_int=features * 2)
+        self.decoder3 = ResidualBlock(features * 8, features * 4)
+
+        self.upconv2 = nn.ConvTranspose2d(features * 4, features * 2, kernel_size=2, stride=2)
+        self.attention2 = AttentionBlock(F_g=features * 2, F_l=features * 2, F_int=features)
+        self.decoder2 = ResidualBlock(features * 4, features * 2)
+
+        self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size=2, stride=2)
+        self.decoder1 = ResidualBlock(features * 2, features)
+
+        # Final Convolution
+        self.conv = nn.Conv2d(features, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(F.max_pool2d(enc1, kernel_size=2, stride=2))
+        enc3 = self.encoder3(F.max_pool2d(enc2, kernel_size=2, stride=2))
+        enc4 = self.encoder4(F.max_pool2d(enc3, kernel_size=2, stride=2))
+
+        # Bottleneck
+        bottleneck = self.bottleneck(F.max_pool2d(enc4, kernel_size=2, stride=2))
+
+        # Decoder
+        dec4 = self.upconv4(bottleneck)
+        att4 = self.attention4(dec4, enc4)
+        dec4 = torch.cat((att4, dec4), dim=1)
+        dec4 = self.decoder4(dec4)
+
+        dec3 = self.upconv3(dec4)
+        att3 = self.attention3(dec3, enc3)
+        dec3 = torch.cat((att3, dec3), dim=1)
+        dec3 = self.decoder3(dec3)
+
+        dec2 = self.upconv2(dec3)
+        att2 = self.attention2(dec2, enc2)
+        dec2 = torch.cat((att2, dec2), dim=1)
+        dec2 = self.decoder2(dec2)
+
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+
+        return self.conv(dec1)
